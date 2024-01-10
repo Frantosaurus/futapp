c26f2190e86156300e85a8ef865c2ad51c53a8a1
Martin Barta <bartamartin1@seznam.cz>
2024-01-10 14:29:11 +0100

    vyber jidla

--- resources/views/vyber_jidla.blade.php
+++ resources/views/vyber_jidla.blade.php
@@ -1,46 +1,46 @@
 @extends('sablony.sablona')
 @section('kontent')
         <section>
-            <!-- <div class="telo">
+            <div class="telo">
                 <div class="karty">
                     <div class="karta">
-                        <img src="/img/IMG_0033.png">
-                        <p>Asijská kuchyně</p>
+                        <img src="/img/logo.png">
+                        <p><a href="http://127.0.0.1:8000/cina">Asijská kuchyně</a></p>
                     </div>
                     <div class="karta">
-                        <img src="/img/IMG_0033.png">
-                        <p>Indická kuchyně</p>
+                        <img src="/img/logo.png">
+                        <p><a href="http://127.0.0.1:8000/indie">Indická kuchyně</a></p>
                     </div>
                     <div class="karta">
-                        <img src="/img/IMG_0033.png">
-                        <p>Česká kuchyně</p>
+                        <img src="/img/logo.png">
+                        <p><a href="http://127.0.0.1:8000/cesko">Česká kuchyně</a></p>
                     </div>
                 </div>
                 <div class="karty">
                     <div class="karta">
-                        <img src="/img/IMG_0033.png">
-                        <p>Kebab</p>
+                        <img src="/img/logo.png">
+                        <p><a href="http://127.0.0.1:8000/kebab">Kebab</a></p>
                     </div>
                     <div class="karta">
-                        <img src="/img/IMG_0033.png">
-                        <p>Italská kuchyně</p>
+                        <img src="/img/logo.png">
+                        <p><a href="http://127.0.0.1:8000/italie">Italská kuchyně</a></p>
                     </div>
                     <div class="karta">
-                        <img src="/img/IMG_0033.png">
-                        <p>Mexická kuchyně</p>
+                        <img src="/img/logo.png">
+                        <p><a href="http://127.0.0.1:8000/mexico">Mexická kuchyně</a></p>
                     </div>
                 </div>
                 <div class="karty">
                     <div class="karta">
-                        <img src="/img/IMG_0033.png">
-                        <p>Menza</p>
+                        <img src="/img/logo.png">
+                        <p><a href="http://127.0.0.1:8000/fastfood">Fastfood</a></p>        
                     </div>
                     <div class="karta">
-                        <img src="/img/IMG_0033.png">
-                        <p></p>
+                        <img src="/img/logo.png">
+                        <p><a href="http://127.0.0.1:8000/menza">Menza</a></p>
                     </div>
                     <div class="karta">
-                        <img src="/img/IMG_0033.png">
+                        <img src="/img/logo.png">
                         <p></p>
                     </div>
                 </div>
@@ -50,11 +50,13 @@
                     <div class="col-auto"><a class="dalsi" href="konkretni_jidlo_mexico.html">Je mi to jedno</a></div>
                 </div>
                 <br>
-            </div> -->
-            @foreach($restaurace as $konkterni_restaurace)
+            </div>
+        </section>
+            </div> 
+           <!-- @foreach($restaurace as $konkterni_restaurace)
             <div>
                 {{$konkterni_restaurace->typ}}
             </div>
-            @endforeach
+            @endforeach-->
         </section>
 @endsection
